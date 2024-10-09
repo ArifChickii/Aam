d9c3b6c20c67820c7f0091d5279437fd3cb831e5
ArifAS12 <arifnaveed1997@gmail.com>
2024-10-09 07:41:02 +0500

    tabbar added

---
+++ Modules/Tabbar/TabbarVC.swift
@@ -0,0 +1,21 @@
+//
+//  TabbarVC.swift
+//  AAM
+//
+//  Created by Arif on 09/10/2024.
+//
+
+import UIKit
+
+class TabbarVC: UITabBarController, Storyboarded {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
